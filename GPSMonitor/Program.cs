49f62e2e624e05e7f4c10e5e98630148fbf876a0
Charlie Hagedorn <cah49@uw.edu>
2016-09-30 17:28:57 -0700

    Adds Satellite-specific SNR count

--- GPSMonitor/Program.cs
+++ GPSMonitor/Program.cs
@@ -18,11 +18,16 @@ namespace GPSMonitor
 		public UInt32 GPSDate; //date as presented by the GPS (140916 is September 14, 2016);
 		public UInt16 satellitesInView; //number of satellites most recently in view
 		public UInt16 fixStatus; //NMEA fix status (1 = no fix, 2 = 2D fix, 3 = 3D fix)
+		public UInt16[] satelliteSNR;
+		public int NumberOfGPSSatellitesInOrbit = 20;
+		public int numSatellitesWithActualSignal;
 
 		private SerialPort mySerial; //The SerialPort object connected to the GPS.
 
 		//Constructor
 		public GPSMonitor( string portName ){
+
+			satelliteSNR = new UInt16[NumberOfGPSSatellitesInOrbit];
 
 			//Close serial port if it's presently open
 			if (mySerial != null)
@@ -54,7 +59,9 @@ namespace GPSMonitor
 		//Function to parse the NMEA sentences
 		//This function is amenable to unit testing.
 		private void ParseString (string s){
-			string[] Words = s.Split (',');
+
+			char[] delimiterChars = { ',', '*'};
+			string[] Words = s.Split ( delimiterChars  );
 
 			//Switch on NMEA sentence-type.
 			switch (Words[0]) {
@@ -65,13 +72,58 @@ namespace GPSMonitor
 
 			case "$GPGSV": //Signal strength and satellite information
 				satellitesInView = UInt16.Parse (Words [3]);
+				int sentenceNumber = Int16.Parse (Words [2]);
+
+				//Figure out how many satellites are in this NMEA sentence
+				int satellitesInThisSentence = satellitesInView - 4 * (sentenceNumber -1 );
+				if( satellitesInThisSentence > 4 ){
+					satellitesInThisSentence = 4;
+				}
+
+				//Update the relevant satellite's SNR
+				for (int satCounter = 0; satCounter < satellitesInThisSentence; satCounter++){
+
+					//Number of the satellite in the SNR array
+					int satIndex = (4*(sentenceNumber-1) + satCounter);
+
+					//Isolate SNR word
+					string satString = Words[ 7 + 4*satCounter];
+
+					//Save SNR to SNR array
+					if (satString.Equals("")){
+						satelliteSNR[satIndex] = 0;
+					}
+					else{
+						satelliteSNR[satIndex] = UInt16.Parse(satString);
+					}
+				}
+
+				//Flush any nonexistent satellits
+				for (int satCounter = satellitesInView; satCounter < NumberOfGPSSatellitesInOrbit; satCounter++){
+					satelliteSNR[satCounter] = 0;
+				}
+
+				//Count non-zero SNRs
+				numSatellitesWithActualSignal = 0;
+				for (int satCounter  = 0 ; satCounter < NumberOfGPSSatellitesInOrbit; satCounter++){
+					if(satelliteSNR[satCounter] > 0){
+						numSatellitesWithActualSignal++;
+					}
+				}
 				break;
 
 			case "$GPRMC": //Position and timing information
 				SystemFixTime = GetCurrentUnixTimestampMillis (); //Do this first to minimize latency
 				GPSTime = Double.Parse (Words [1]);
 				GPSDate = UInt32.Parse (Words [9]);
-				Console.WriteLine (SystemFixTime + "\t" + GPSTime + "\t" + GPSDate + "\t" + satellitesInView + "\t" + fixStatus);
+				Console.WriteLine (SystemFixTime + "\t" + GPSTime + "\t" + GPSDate + "\t" + satellitesInView + "\t" + fixStatus + "\t" + numSatellitesWithActualSignal);
+
+				//Debug output for satellite SNR
+				//for( int satCounter = 0; satCounter < NumberOfGPSSatellitesInOrbit; satCounter++){
+				//	Console.Write("\t" + satelliteSNR[satCounter]);
+				//}
+				//Console.WriteLine("");
+
 				break;
 
 			default:
